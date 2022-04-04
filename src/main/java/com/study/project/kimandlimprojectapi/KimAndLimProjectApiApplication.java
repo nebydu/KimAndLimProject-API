ee5486e541855232c94e4e91fd653f4df74940d9
nebydu <hongil.lim@samsung.com>
2022-04-04 14:25:32 +0900

    첫커밋

---
+++ src/main/java/com/study/project/kimandlimprojectapi/KimAndLimProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.study.project.kimandlimprojectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimAndLimProjectApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KimAndLimProjectApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/project/kimandlimprojectapi/KimAndLimProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.project.kimandlimprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimAndLimProjectApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
